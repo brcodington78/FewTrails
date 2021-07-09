f42c9702c068124d4309ea49c130693516276aec
brcodington78 <brcodington@gmail.com>
2021-07-09 16:10:42 -0700

    added component skeleton of trails_page

---
+++ frontend/components/trails/trails_page.jsx
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+import {logout} from '../actions/session_actions';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+class TrailPage extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        return (
+            null
+            // <p></p>
+            // <SearchBar />
+            // <Banner />
+            // <NearbyTrails />
+            // <TrailDescription />
+            // <TrailNav />
+
+        )
+    }
+}
+
---
+++ frontend/components/trails/trails_page_container.jsx
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+import {logout} from '../actions/session_actions';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import TrailPage from './trails_page'
+
+
