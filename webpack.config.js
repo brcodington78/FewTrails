4d8fd96eaf26a119aa46130d2e724075775440f9
brcodington78 <brcodington@gmail.com>
2021-09-07 12:19:48 -0700

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "app", "assets", "javascripts"
         ),
-        filename: 'bundle.js'
+        filename: './bundle.js'
     },
     resolve: {
         extensions: ['.js', '.jsx', '.*']
